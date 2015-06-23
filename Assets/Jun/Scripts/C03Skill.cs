139b433f920a9212742a5536719892cc2bb5f19c
Novicecalf <djinni75@naver.com>
2015-06-23 17:44:28 +0900

    0623 전투 시스템 끝

--- Assets/Jun/Scripts/C03Skill.cs
+++ Assets/Jun/Scripts/C03Skill.cs
@@ -3,15 +3,15 @@ using System.Collections;
 
 public class C03Skill : MonoBehaviour
 {
-	public float SK03HP;
-	public float SK03HPT;
-	public float SK03HPC;
+	public float SK03ATS;
+	public float SK03ATST;
+	public float SK03ATSC;
 		
-	public float SK01DEF;
-	public float SK01DEFT;
-	public float SK01DEFC;
+	public float SK03AT;
+	public float SK03ATT;
+	public float SK03ATC;
 	
-	public float SK01DOD;
-	public float SK01DODT;
-	public float SK01DODC;
+	public float SK03AS;
+	public float SK03AST;
+	public float SK03ASC;
 }
